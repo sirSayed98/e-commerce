6d7663c85a45ffceebc3e1cfc0ffd71ec1f4a54f
mohamed sayed <mohamed.sayed8899@gmail.com>
2020-10-01 12:10:23 +0200

    add HomeScreen and Product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
+import HomeScreen from './screens/HomeScreen'
 function App() {
   return (
    <>
     <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome To ProShop</h1>
+        <HomeScreen />
         </Container>
       </main>
       <Footer />
