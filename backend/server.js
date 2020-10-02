121ed6505435b87b72b159fd87a06863d444b2dc
mohamed sayed <mohamed.sayed8899@gmail.com>
2020-10-02 15:04:41 +0200

    add env file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
-const express =require('express');
-const app=express();
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config('.env');
 
-app.listen(5000, () => {
-    console.log('App listening on port 5000!');
+
+
+const Port = process.env.PORT || 5000;
+app.listen(Port, () => {
+    console.log(`App listening on port ${Port} on ${process.env.NODE_ENV} mode`);
 });
