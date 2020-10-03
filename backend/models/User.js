c79d00d81642cfef3fc6029ef84fed3efc46e698
mohamed sayed <mohamed.sayed8899@gmail.com>
2020-10-03 11:03:54 +0200

    add user model

---
+++ backend/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = required('mongoose');
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        isAdmin: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User;
