c90a5318662aea36865beeb521df2b7c3793eb67
mohamed sayed <mohamed.sayed8899@gmail.com>
2020-10-03 15:35:44 +0200

    add store and install redux packages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './bootstrap.min.css';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
