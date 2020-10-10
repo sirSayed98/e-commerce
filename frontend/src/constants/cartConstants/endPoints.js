e29c3597ec27eae91d5b76ea109cfd7297531852
mohamed sayed <mohamed.sayed8899@gmail.com>
2020-10-10 11:15:20 +0200

    add cartActions and reducer

---
+++ frontend/src/constants/cartConstants/endPoints.js
@@ -0,0 +1,1 @@
+export const GET_PRODUCT = 'http://localhost:5000/api/v1/products/'
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,8 +10,13 @@ const reducer = combineReducers({
   productDetails: productDetailsReducer,
 })
 
-const initialState = {}
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : []
 
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+}
 const middleware = [thunk]
 
 const store = createStore(
