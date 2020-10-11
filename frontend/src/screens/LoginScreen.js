a5f6be1a3513c3340a21f67c78b234347f00878e
mohamed sayed <mohamed.sayed8899@gmail.com>
2020-10-11 11:42:24 +0200

    add Username and logout

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -10,7 +10,7 @@ import { login } from '../actions/userActions'
 const LoginScreen = ({ location, history }) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-
+    
     const dispatch = useDispatch()
 
     const userLogin = useSelector((state) => state.userLogin)
