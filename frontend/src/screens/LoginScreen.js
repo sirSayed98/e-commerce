70db005f95e26d699e16f709445c3f598fdceca3
sirSayed98 <mohamed.sayed8899@hotmail.com>
2021-03-28 13:57:38 +0200

    add checkout steps Component

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -19,7 +19,6 @@ const LoginScreen = ({ location, history }) => {
     const redirect = location.search ? location.search.split('=')[1] : '/'
 
     useEffect(() => {
-        
         if (userInfo) {
             history.push(redirect)
         }
