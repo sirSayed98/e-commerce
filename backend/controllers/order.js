3d427be5052448e952fad45f36368c4cc80b95eb
sirSayed98 <mohamed.sayed8899@hotmail.com>
2021-03-28 22:17:50 +0200

    add get single product endpoint

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -1,43 +1,56 @@
-const express = require('express');
-const asyncHandler = require('express-async-handler');
-const Order = require('../models/Order')
-const ErrorResponse = require('../utils/errorResponse');
-
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+const Order = require("../models/Order");
+const ErrorResponse = require("../utils/errorResponse");
 
 // @desc    add order
 // @route   POST /api/v1/order
 // @access  Private
 
 exports.addOrderItems = asyncHandler(async (req, res, next) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
 
-    console.log("_____________________________________")
-    const {
-        orderItems,
-        shippingAddress,
-        paymentMethod,
-        itemsPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice,
-      } = req.body
-    
-      if (orderItems && orderItems.length === 0) {
-        res.status(400)
-        return next(new ErrorResponse('no Items found', 400))
-      } else {
-        const order = new Order({
-          orderItems,
-          user: req.user._id,
-          shippingAddress,
-          paymentMethod,
-          itemsPrice,
-          taxPrice,
-          shippingPrice,
-          totalPrice,
-        })
-    
-        const createdOrder = await order.save()
-    
-        res.status(201).json(createdOrder)
-    }
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    return next(new ErrorResponse("no Items found", 400));
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
+});
+
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+exports.getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    return next(new ErrorResponse("no Items found", 404));
+  }
 });
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { addOrderItems } = require("../controllers/order");
+const { addOrderItems,getOrderById } = require("../controllers/order");
 
 const { protect } = require("../middleware/auth");
 
 
 router.route("/").post(protect, addOrderItems);
+router.route('/:id').get(protect, getOrderById)
 
 module.exports = router;
