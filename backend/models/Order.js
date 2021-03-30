43594c0f75e73284657499e674e387bfedc6fd4b
sirSayed98 <mohamed.sayed8899@hotmail.com>
2021-03-30 18:30:32 +0200

    add add order screen details  ordertItems in order model - fixbug in orderCart

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -51,6 +51,11 @@ const orderSchema = mongoose.Schema(
       required: true,
       default: 0.0,
     },
+    itemsPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
     isPaid: {
       type: Boolean,
       required: true,
