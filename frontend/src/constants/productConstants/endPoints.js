fda198dd0afa2b7e9388295a61e7e8ead0b91ded
sirSayed98 <mohamed.sayed8899@hotmail.com>
2021-04-01 15:54:35 +0200

    -front- add delete functionality action and reducer

--- frontend/src/constants/productConstants/endPoints.js
+++ frontend/src/constants/productConstants/endPoints.js
@@ -1,2 +1,3 @@
 export const GET_PRODUCTS = 'http://localhost:5000/api/v1/products/'
 export const GET_PRODUCT = 'http://localhost:5000/api/v1/products/'
+export const DELETE_PRODUCT = 'http://localhost:5000/api/v1/products/'
--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,7 +4,7 @@ import { Table, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listProducts } from '../actions/productActions'
+import { listProducts,deleteProduct } from '../actions/productActions'
 
 const ProductListScreen = ({ history, match }) => {
   const dispatch = useDispatch()
@@ -15,17 +15,24 @@ const ProductListScreen = ({ history, match }) => {
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
 
+  const productDelete = useSelector((state) => state.productDelete)
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = productDelete
+
   useEffect(() => {
     if (userInfo && userInfo.role === "admin") {
       dispatch(listProducts())
     } else {
       history.push('/login')
     }
-  }, [dispatch, history, userInfo])
+  }, [dispatch, history, userInfo,successDelete])
 
   const deleteHandler = (id) => {
     if (window.confirm('Are you sure')) {
-      // DELETE PRODUCTS
+      dispatch(deleteProduct(id))
     }
   }
 
@@ -45,6 +52,10 @@ const ProductListScreen = ({ history, match }) => {
           </Button>
         </Col>
       </Row>
+
+      {loadingDelete && <Loader />}
+      {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+      
       {loading ? (
         <Loader />
       ) : error ? (
