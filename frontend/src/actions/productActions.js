6bb11f927c43b249d9f619ed2283b95764090f03
sirSayed98 <mohamed.sayed8899@hotmail.com>
2021-04-02 15:13:34 +0200

    -front- add product screen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -15,6 +15,9 @@ import {
   PRODUCT_UPDATE_REQUEST,
   PRODUCT_UPDATE_SUCCESS,
   PRODUCT_UPDATE_FAIL,
+  PRODUCT_CREATE_REVIEW_REQUEST,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_CREATE_REVIEW_FAIL,
 } from "../constants/productConstants/types";
 
 import {
@@ -22,6 +25,7 @@ import {
   GET_PRODUCT,
   DELETE_PRODUCT,
   CREATE_PRODUCT,
+  MAKE_PRODUCT_REVIEW,
 } from "../constants/productConstants/endPoints";
 
 export const listProducts = () => async (dispatch) => {
@@ -163,6 +167,49 @@ export const updateProduct = (product) => async (dispatch, getState) => {
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const createProductReview = (productId, review) => async (
+  dispatch,
+  getState
+) => {
+  try {
+    dispatch({
+      type: PRODUCT_CREATE_REVIEW_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    review.userID = userInfo.id;
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    await axios.post(
+      MAKE_PRODUCT_REVIEW + `${productId}/reviews`,
+      review,
+      config
+    );
+
+    dispatch({
+      type: PRODUCT_CREATE_REVIEW_SUCCESS,
+    });
+  } catch (error) {
+    console.log(error.response.data.error);
+    dispatch({
+      type: PRODUCT_CREATE_REVIEW_FAIL,
+      payload:
+        error.response && error.response.data.error
+          ? error.response.data.error
           : error.message,
     });
   }
